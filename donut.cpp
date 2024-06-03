3c55be2528710cab8c3f09f71035cfbcb1506ce1
dantedellatogna <dellatogna.io@gmail.com>
2024-06-03 03:24:09 -0500

    Improved light-system To do: - Find out why the back side of the Torus is lit and fix it - Make it rotate around the x and z axes

--- donut.cpp
+++ donut.cpp
@@ -9,13 +9,13 @@
 void clear_screen();
 
 void gotoxy(int x, int y);
-void light_system(std::vector<double> a, std::vector<double> light, double light_angle);
-void light_system2(std::vector<double> a, std::vector<double> light, double light_angle);
+void light_system(std::vector<double> normal, std::vector<double> light, double light_angle);
+std::vector<double> normal_surface(std::vector<double> a, double theta, double phi);
 
 int main()
 {
-    double theta{};
-    double phi{};
+    double theta{}; // Big circle
+    double phi{};   // small circle
 
     double R{30};
     double r{15};
@@ -27,13 +27,17 @@ int main()
     std::vector<double> d(3);        // Camera transformation
     std::vector<double> b(2);        // Projection on 2D plane
 
+    std::vector<double> normal(3); // Normal vector for each point in the surface
+
     double light_angle{45};
-    std::vector<double> light{0, -1, 1};
+    std::vector<double> light{0, 1, 1};
     // std::vector<double> light{1, 0, 1};
 
+    std::vector<double> x_rotation_transform{0, 0, 0};
     std::vector<double> y_rotation_transform{0, 0, 0};
+    std::vector<double> z_rotation_transform{0, 0, 0};
 
-    std::vector<double> rotation_angle{0, 0, 0}; // Rotation angle
+    double rotation_angle; // Rotation angle
 
     while (true)
     {
@@ -45,19 +49,25 @@ int main()
                 y = sin(theta) * (R + r * cos(phi));
                 z = r * sin(phi);
 
-                // rotation x
-                y_rotation_transform[0] = x * cos(rotation_angle[1]) - z * sin(rotation_angle[1]);
-                y_rotation_transform[2] = x * sin(rotation_angle[1]) + z * cos(rotation_angle[1]);
+                // rotation x_axis
+                // x_rotation_transform[1] = y * cos(rotation_angle) - z * sin(rotation_angle);
+                // x_rotation_transform[2] = y * sin(rotation_angle) + z * cos(rotation_angle);
+
+                // rotation y-axis
+                y_rotation_transform[0] = x * cos(rotation_angle) - z * sin(rotation_angle);
+                y_rotation_transform[2] = x * sin(rotation_angle) + z * cos(rotation_angle);
+
+                // rotation z-axis
+                // z_rotation_transform[0] = x * cos(rotation_angle) - y * sin(rotation_angle);
+                // z_rotation_transform[1] = x * sin(rotation_angle) + y * cos(rotation_angle);
 
                 // projection
-                a[0] = y_rotation_transform[0];
+                a[0] = x_rotation_transform[0] + y_rotation_transform[0] + z_rotation_transform[0];
                 a[1] = y;
-                a[2] = y_rotation_transform[2] + 70;
-                /*
-                a[0] = x;
-                a[1] = y;
-                a[2] = z + 30;
-                */
+                a[2] = x_rotation_transform[2] + y_rotation_transform[2] + z_rotation_transform[2] + 70;
+
+                // Calculate normal vector
+                normal = normal_surface(a, theta, phi);
 
                 d[0] = a[0] - c[0];
                 d[1] = a[1] - c[1];
@@ -66,26 +76,17 @@ int main()
                 b[0] = (e[2] / d[2]) * d[0] - e[0];
                 b[1] = (e[2] / d[2]) * d[1] - e[1];
 
-                // std::cout<<a[0]<<std::endl;
-
-                // gotoxy((x + 100) / 2, (y + 50) / 4);
                 gotoxy((b[0] + 100) / 2, (b[1] + 70) / 4);
 
                 // Light System
-                //light_system(a, light, light_angle);
-                light_system2(a, light, light_angle);
+                light_system(normal, light, light_angle);
                 // usleep(300);
             }
         }
-        // std::cout << "\033[2J";
-        // light_angle += 0.5;
-        // light[0] = cos(light_angle);
-        // light[1] = -sin(light_angle);
-        // light[2] = sin(light_angle);
 
-        if (rotation_angle[1] == 360)
-            rotation_angle[1] = 0;
-        rotation_angle[1] += 0.1;
+        if (rotation_angle == 360)
+            rotation_angle = 0;
+        rotation_angle += 0.1;
         clear_screen();
     }
 
@@ -108,46 +109,55 @@ void gotoxy(int x, int y)
     printf("%c[%d;%df", 0x1B, y, x);
 }
 
-void light_system(std::vector<double> a, std::vector<double> light, double light_angle)
+void light_system(std::vector<double> normal, std::vector<double> light, double light_angle)
 {
 
-    double dot_al{}; // Dot product of 3D Object and Light vectors
-    double a_mag{};  // 3D Object vector magnitude
-    double l_mag{};  // Ligth vector magnitude
+    double dot_product{}; // Dot product of Normal surface and Light vectors
+    dot_product = normal[0] * light[0] + normal[1] * light[1] + normal[2] * light[2];
 
-    double angle_between{};
-
-    dot_al = a[0] * light[0] + a[1] * light[1] + a[2] * light[2];
-    a_mag = sqrt((pow(a[0], 2) + pow(a[1], 2) + pow(a[2], 2)));
-    l_mag = sqrt((pow(light[0], 2) + pow(light[1], 2) + pow(light[2], 2)));
-
-    angle_between = acos(dot_al / (a_mag * l_mag)); // returns radians. Need to transform it to degrees.
-    angle_between = angle_between * 180 / M_PI;
-    // std::cout <<angle_between<<std::endl;
-
-    if (angle_between >= 70)
+    if (dot_product < -1)
         std::cout << ".";
-    else if (angle_between >= 50)
+    else if (dot_product < 0)
         std::cout << ",";
-    else if (angle_between >= 30)
-        std::cout << "~";
-    else if (angle_between >= 10)
+    else if (dot_product < 0.2)
+        std::cout << "-";
+    else if (dot_product < 1)
+        std::cout << ";";
+    else if (dot_product < 1.2)
         std::cout << "#";
-    else if (angle_between < 10)
+    else if (dot_product >= 1.2)
         std::cout << "@";
+    // std::cout << dot_product << std::endl;
+    // usleep(3000);
 }
 
-void light_system2(std::vector<double> a, std::vector<double> light, double light_angle)
+std::vector<double> normal_surface(std::vector<double> a, double theta, double phi)
 {
+    // theta -> Big circle; phi -> Small circle
+    std::vector<double> tangent_R(3); // Tangent vector with respect to big circle
+    std::vector<double> tangent_r(3); // Tangent vector with respectr to little circle
+    std::vector<double> normal(3);
 
-    double dot_al{}; // Dot product of 3D Object and Light vectors
-    dot_al = a[0] * light[0] + a[1] * light[1] + a[2] * light[2];
+    double length{};
 
-    if (dot_al >= 100)
-        std::cout << "$";
-    else if (dot_al < 100)
-        std::cout << ".";
-    
-    //std::cout << dot_al << std::endl;
-    //usleep(3000);
+    tangent_R[0] = -sin(theta);
+    tangent_R[1] = cos(theta);
+    tangent_R[2] = 0;
+
+    tangent_r[0] = cos(theta) * (-sin(phi));
+    tangent_r[1] = sin(theta) * (-sin(phi));
+    tangent_r[2] = cos(phi);
+
+    // Normal vector is the the cross-product of the tangent vectors
+    normal[0] = tangent_R[1] * tangent_r[2] - tangent_R[2] * tangent_r[1];
+    normal[1] = tangent_R[0] * tangent_r[2] - tangent_R[2] * tangent_r[0];
+    normal[2] = tangent_R[0] * tangent_r[1] - tangent_R[1] * tangent_r[0];
+
+    // Normalize normal vector
+    length = sqrt(normal[0] * normal[0] + normal[1] * normal[1] + normal[2] * normal[2]);
+    normal[0] /= length;
+    normal[1] /= length;
+    normal[2] /= length;
+
+    return normal;
 }
