defd8c6b3b3716813a20bb12e0810fb01b29a34c
dantedellatogna <dellatogna.io@gmail.com>
2024-06-08 18:12:51 -0500

    small changes

--- donut.cpp
+++ donut.cpp
@@ -28,19 +28,19 @@ int main()
     float x{}, y{}, z{};
     std::vector<float> a{0, 0, 0};  // 3D Object
     std::vector<float> c{0, 0, 0};  // Camera
-    std::vector<float> e{0, 0, 70}; // Plane
+    std::vector<float> e{0, 0, 40}; // Plane
     std::vector<float> d(3);        // Camera transformation
     std::vector<float> b(2);        // Projection on 2D plane
 
     std::vector<float> normal(3); // Normal vector for each point in the surface
 
-    std::vector<float> light{0, 1, 1};
+    std::vector<float> light{1, -1, 1};
     // std::vector<double> light{1, 0, 1};
 
-    std::vector<float> origin{0, 0, 100};
-    std::vector<float> x_rotation_transform{0, 0, 0};
-    std::vector<float> y_rotation_transform{0, 0, 0};
-    std::vector<float> z_rotation_transform{0, 0, 0};
+    std::vector<float> origin{0, 0, 60};
+    std::vector<float> x_rotation_transform(3);
+    std::vector<float> y_rotation_transform(3);
+    std::vector<float> z_rotation_transform(3);
 
     float rotation_angle{0}; // Rotation angle
     float cosrotation{};
@@ -55,6 +55,7 @@ int main()
         {
             costheta = cos(theta);
             sintheta = sin(theta);
+
             for (phi = 0; phi < tau; phi += 0.1)
             {
                 cosphi = cos(phi);
@@ -67,6 +68,15 @@ int main()
                 cosrotation = cos(rotation_angle);
                 sinrotation = sin(rotation_angle);
 
+                // test
+                y_rotation_transform[0] = x * cosrotation - z * sinrotation;
+                y_rotation_transform[2] = x * sinrotation + z * cosrotation;
+
+                a[0] = y_rotation_transform[0];
+                a[1] = y;
+                a[2] = y_rotation_transform[2] + origin[2];
+
+                /*
                 // rotation x_axis
                 x_rotation_transform[0] = x;
                 x_rotation_transform[1] = y * cosrotation - z * sinrotation;
@@ -86,6 +96,7 @@ int main()
                 a[0] = z_rotation_transform[0] + origin[0];
                 a[1] = z_rotation_transform[1] + origin[1];
                 a[2] = z_rotation_transform[2] + origin[2];
+                                */
                 // Calculate normal vector
                 normal = normal_surface(costheta, sintheta, cosphi, sinphi);
 
@@ -100,12 +111,19 @@ int main()
 
                 // Light System
                 light_system(normal, light);
+                // usleep(500000);
+
+                // gotoxy(0, 8);
+                // std::cout << "Normal X: " << normal[0] << " Normal Y: " << normal[1] << " Normal Z: " << normal[2] << std::endl;
+                // std::cout << "Light X: " << light[0] << " Light Y: " << light[1] << " Light Z: " << light[2];
             }
         }
 
         if (rotation_angle >= tau)
-            rotation_angle = 0;
+            rotation_angle -= tau;
         rotation_angle += 0.05;
+        gotoxy(0, 10);
+        std::cout << "Rotation angle: " << rotation_angle << std::endl;
         usleep(50000);
         // clear_screen();
     }
@@ -134,18 +152,18 @@ void light_system(std::vector<float> normal, std::vector<float> light)
     float dot_product{}; // Dot product of Normal surface and Light vectors
     dot_product = normal[0] * light[0] + normal[1] * light[1] + normal[2] * light[2];
 
-    if (dot_product <= 0)
+    if (dot_product <= 0) //|| dot_product > 0)
         std::cout << ".";
     else if (dot_product < 0.2)
         std::cout << "-";
     else if (dot_product < 0.5)
         std::cout << "~";
     else if (dot_product < 0.7)
-        std::cout << "~";
+        std::cout << ":";
     else if (dot_product < 0.9)
         std::cout << ";";
     else if (dot_product < 1)
-        std::cout << "!";
+        std::cout << "$";
     else if (dot_product < 1.2)
         std::cout << "#";
     else if (dot_product >= 1.2)
